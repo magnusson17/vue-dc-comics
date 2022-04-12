f426b11f8368ba265a09e9305b2d2abad9619c81
Ga <gabri.magni.97@gmail.com>
2022-04-12 15:23:01 +0200

    header

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <HeaderComp></HeaderComp>
+  </div>
+</template>
+
+<script>
+import HeaderComp from './components/HeaderComp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComp,
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  // margin-top: 60px;
+}
+</style>
+
+<!-- Create un nuovo progetto utilizzando Vue CLI e definite i componenti necessari per strutturare il layout come da screenshot allegato.
+
+Quando la struttura a macroblocchi è pronta, popolate le voci di menu dinamicamente usando i data del componente.
+Per oggi diamo priorità alla struttura: quando è tutto bello solido, passiamo al Sass!
+
+Bonus:
+Creare un componente aggiuntivo per gestire la fascia azzurra con le icone -->
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,47 @@
+<template>
+  <header>
+      <div>
+          <img src="../assets/img/dc-logo.png" alt="dc-logo">
+      </div>
+      <div>
+          <ul>
+              <li>CHARACTERS</li>
+              <li>COMICS</li>
+              <li>MOVIES</li>
+              <li>TV</li>
+              <li>GAMES</li>
+              <li>COLLECTIBLES</li>
+              <li>VIDEOS</li>
+              <li>FANS</li>
+              <li>NEWS</li>
+              <li>SHOP</li>
+          </ul>
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComp',
+}
+</script>
+
+<style scoped lang="scss">
+header {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+}
+ul {
+    list-style: none;
+    display: flex;
+    li {
+        text-align: left;
+        margin: 0 20px;
+        font-size: 0.5em;
+        &:hover {
+            color: blue;
+        }
+    }
+}
+</style>
