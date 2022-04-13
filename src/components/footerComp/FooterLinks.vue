771b9bd494e777070a94802d2dd737024ad88915
Ga <gabri.magni.97@gmail.com>
2022-04-13 12:30:34 +0200

    FooterLinks dinamic list draft

--- src/components/footerComp/FooterLinks.vue
+++ src/components/footerComp/FooterLinks.vue
@@ -1,11 +1,15 @@
 <template>
 <div>
     <ul>
-        <li><img src="../../assets/img/buy-comics-digital-comics.png" alt="dc-img">digital comics</li>
+        <li v-for="(element, index) in listaLink" :key="index">
+            <img :src="`../../assets/img/${element.img}`" alt="dc-img">
+            <span>{{element.title}}</span>
+        </li>
+        <!-- <li><img src="../../assets/img/buy-comics-digital-comics.png" alt="dc-img">digital comics</li>
         <li><img src="../../assets/img/buy-comics-merchandise.png" alt="dc-img">dc merchandise</li>
         <li><img src="../../assets/img/buy-comics-subscriptions.png" alt="dc-img">subscription</li>
         <li><img src="../../assets/img/buy-comics-shop-locator.png" alt="dc-img">comic shop locator</li>
-        <li><img src="../../assets/img/buy-dc-power-visa.svg" alt="dc-img">dc power visa</li>
+        <li><img src="../../assets/img/buy-dc-power-visa.svg" alt="dc-img">dc power visa</li> -->
     </ul>
 </div>
 </template>
@@ -13,6 +17,33 @@
 <script>
 export default {
   name: 'FooterLinks',
+
+  data() {
+      return {
+          listaLink: [
+              {
+                  title: 'digital comics',
+                  img: 'buy-comics-digital-comics.png',
+              },
+              {
+                  title: 'dc merchandise',
+                  img: 'buy-comics-merchandise.png',
+              },
+              {
+                  title: 'subscription',
+                  img: 'buy-comics-subscriptions.png',
+              },
+              {
+                  title: 'comic shop locator',
+                  img: 'buy-comics-shop-locator.png',
+              },
+              {
+                  title: 'dc power visa',
+                  img: 'buy-dc-power-visa.svg',
+              },
+          ]
+      }
+  }
 }
 </script>
 
